adad8a7519106705afede0c7fcb794933887055d
Eddy Hipo <eddy.hipo@epn.edu.ec>
2020-09-13 22:07:32 -0500

    agregar comentario a firebase

--- src/Pages/Foro.js
+++ src/Pages/Foro.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {Card, Col, Input, Row, Comment, Tooltip, Button, Form} from "antd";
+import {Card, Col, Input, Row, Comment, Tooltip, Button, Form, message} from "antd";
 import '../styles/foro.css';
 import HeaderForos from "../components/HeaderForos";
 import Foot from "../components/Foot";
@@ -13,28 +13,31 @@ const Foro = ( {dataForums}) => {
     const [dataForum, setDataForum] = useState({key: '',
         title: '',
         user: '',
+        userId: '',
         date: '',
         message:'',
         comments: []
+    });
+
+    const [dataProfile, setDataProfile] = useState({key: '',
+        address: '',
+        email: '',
+        name: '',
+        phone:''
     });
 
     /*const { Search } = Input;*/
     //console.log('dataForums-foro-pasados', {location});
     //const {data} = this.props.location.state;
 
-    const onFinish = values => {
-        console.log(values);
-    };
-
-    console.log('dataForums-foro-pasados', dataForums);
-
     useEffect( () => {
         const getDataForum  = async () => {
-            FIREBASE.db.ref('forums/3').on('value', (snapshot) => {
+            FIREBASE.db.ref('forums/5').on('value', (snapshot) => {
                 console.log('snapshot', snapshot.val());
                 const forum = snapshot.val();
                 const forumId = snapshot.key;
                 const listCommentsData = [];
+
                 forum.comments.forEach( (data) => {
                     //console.log('datos de comentarios', data);
                     const comment = data;
@@ -54,6 +57,7 @@ const Foro = ( {dataForums}) => {
                     user: forum.name,
                     date: forum.date,
                     message:forum.message,
+                    userId: forum.userid,
                     comments: listCommentsData,
                 };
                 //console.log('forumdata', forumData);
@@ -61,10 +65,48 @@ const Foro = ( {dataForums}) => {
 
             });
         };
+
+        const getDataProfile  = async () => {
+            FIREBASE.db.ref('users/3').on('value', (snapshot) => {
+                //console.log('snapshot', snapshot.val());
+                const profile = snapshot.val();
+                const profileId = snapshot.key;
+                const profileData = {key: profileId,
+                    address: profile.address,
+                    email: profile.email,
+                    name: profile.name,
+                    phone:profile.phone
+                };
+                //console.log('forumdata', forumData);
+                setDataProfile(profileData);
+
+            });
+        };
+
+        getDataProfile();
         getDataForum();
     }, []);
 
-    console.log('dataForum', dataForum);
+    const onFinish = async ( values ) => {
+        //console.log('valores pasados al click ', values);
+        const today = new Date();
+        const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+        console.log('comentario: ', values.comment);
+        console.log('llave del foro: ', dataForum.key);
+        console.log('usuario: ', dataProfile.name);
+        console.log('Fecha: ', date);
+        console.log('usuario id: ',  dataProfile.key);
+
+        await FIREBASE.db.ref(`forums/${dataForum.key}/comments/`).push({
+            comment: values.comment,
+            date: date,
+            name: dataProfile.name,
+            userid: dataProfile.key
+        });
+        message.success('Los datos se actualizarón corectamente');
+    };
+
     return (
         <>
             <HeaderForos/>
@@ -138,7 +180,7 @@ const Foro = ( {dataForums}) => {
                         <p className="tam-titu2"><strong>Agregar Comentario</strong></p>
                         <Card className="colorBaseB internal-box-size " bordered={true} align="left">
                             <Form  name="nest-messages" onFinish={onFinish} >
-                                <Form.Item name={['user', 'introduction']}>
+                                <Form.Item name={['comment']}>
                                     <Input.TextArea />
                                 </Form.Item>
                                 <Form.Item >
